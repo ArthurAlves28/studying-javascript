bf5d2693f4db18888a757736dd4fee2af2c2bdea
ArthurAlves28 <arthuralvessa@gmail.com>
2020-09-11 21:22:59 -0300

    Início do Capítulo 8, explorando o Node.js. Exportando e importando módulos.

---
+++ code3r/08-node/123-moduloA.js
@@ -0,0 +1,6 @@
+// 1ª forma de importação
+this.ola = 'Fala pessoal'
+// 2ª forma de importação
+exports.bemVindo = 'Bem vindo ao node!'
+// 3ª forma de importação (classico)
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ code3r/08-node/123-moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./123-moduloA') // Acessando o moduloA.js
+const moduloB = require('./123-moduloB') // acessando o moduloB.js
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
